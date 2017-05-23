081ab401c31d90e077163adf56af06745c2f4926
Marek <mpietrewicz@gmail.com>
2017-05-23 22:19:51 +0200

    Realizacja #2 poprawki

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Timer timer = new Timer();
         timer.start();
+        System.out.println("start");
+        System.out.println("2");
     }
 }
